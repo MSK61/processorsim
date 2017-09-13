f9801adb1bbf3a352b47dd0dea3ea1ce3b187b28
Mohammed El-Afifi <Mohammed_ElAfifi@yahoo.com>
2017-09-13 21:15:10 +0200

    testing exceeding unit capacities

--- test/test_sim.py
+++ test/test_sim.py
@@ -87,6 +87,8 @@ class TestSim:
          "dualCoreMemALUProcessor.yaml", [{"core 2": [0]}]),
         ("2InstructionProgram.asm", "2WideALUProcessor.yaml",
          [{"fullSys": [0, 1]}]),
+        ("3InstructionProgram.asm", "2WideALUProcessor.yaml",
+         [{"fullSys": [0, 1]}, {"fullSys": [2]}]),
         ("instructionWithOneSpaceBeforeOperandsAndNoSpacesAroundComma.asm",
          "twoConnectedUnitsProcessor.yaml",
          [{"input": [0]}, {"output": [0]}])])
