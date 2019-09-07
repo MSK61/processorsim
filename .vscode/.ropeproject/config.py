23cbbb277bae4f715c8e91983a2f426ffeeeb6a2
Mohammed El-Afifi <Mohammed_ElAfifi@yahoo.com>
2019-09-07 02:04:03 +0200

    adding copyright notice to a file

--- .vscode/.ropeproject/config.py
+++ .vscode/.ropeproject/config.py
@@ -1,3 +1,42 @@
+# -*- coding: utf-8 -*-
+
+"""rope configuration"""
+
+############################################################
+#
+# Copyright 2017, 2019 Mohammed El-Afifi
+# This file is part of processorSim.
+#
+# processorSim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# processorSim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with processorSim.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# program:      processor simulator
+#
+# file:         config.py
+#
+# function:     rope project configuration
+#
+# description:  rope configuration settings
+#
+# author:       Mohammed El-Afifi (ME)
+#
+# environment:  Visual Studdio Code 1.38.0, python 3.7.4, Fedora release
+#               30 (Thirty)
+#
+# notes:        This is a private program.
+#
+############################################################
 # The default ``config.py``
 # flake8: noqa
 
