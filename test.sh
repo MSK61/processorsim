8b3ba0f63496a7c3c30efa3b6f3d8ae9da3b073f
Mohammed El-Afifi <Mohammed_ElAfifi@yahoo.com>
2020-01-04 20:51:18 +0200

    Revert "Suppress exiting on first error"
    This reverts commit e45a3c91486181e248382e7953716010eac12b05.

--- test.sh
+++ test.sh
@@ -33,6 +33,7 @@
 # notes:        This is a private program.
 #
 ############################################################
+set -e
 pytest --pycodestyle --cov src --flakes --pylint $*
 cd src
 pytest -m pylint --pylint $*
