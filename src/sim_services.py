81ea552079164fbec8d111f0b001566bbad0b015
Mohammed El-Afifi <Mohammed_ElAfifi@yahoo.com>
2020-02-11 22:37:45 +0100

    Use more_itertools.rlocate
    Because:
    It makes intent a bit clearer.

--- src/sim_services.py
+++ src/sim_services.py
@@ -496,7 +496,7 @@ def _flush_output(out_instr_lst: MutableSequence[InstrState]) -> None:
     `out_instr_lst` is the list of instructions in the output unit.
 
     """
-    instr_indices = reversed(range(len(out_instr_lst)))
+    instr_indices = more_itertools.rlocate(out_instr_lst, lambda _: True)
 
     for instr_index in instr_indices:
         if out_instr_lst[instr_index].stalled == StallState.NO_STALL:
