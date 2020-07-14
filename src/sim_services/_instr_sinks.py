8c523b2757ec6920039bb562b298dcb7dd4ec218
Mohammed El-Afifi <Mohammed_ElAfifi@yahoo.com>
2020-07-14 16:52:35 +0200

    Move common method to a base class
    Because:
    OutSink and UnitSink classes in sim_services._instr_sinks share a lot of
    duplicated code.
    
    I moved all duplicated code to methods in their base class _INstrSink.

--- src/sim_services/_instr_sinks.py
+++ src/sim_services/_instr_sinks.py
@@ -39,14 +39,14 @@
 ############################################################
 
 import abc
+from abc import abstractmethod
 import heapq
 import itertools
-from itertools import chain
 import typing
 from typing import Collection, Iterable, Iterator
 
 import attr
-from more_itertools import locate
+import more_itertools
 
 from container_utils import BagValDict
 from processor_utils import units
@@ -91,15 +91,81 @@ class InstrSink(abc.ABC):
 
     """Instruction sink"""
 
-    @abc.abstractmethod
     def fill_unit(self, util_info: BagValDict[ICaseString, InstrState],
                   mem_busy: bool) -> UnitFillStatus:
-        """Fill an output with instructions from its predecessors.
+        """Fill this sink with instructions from its donors.
 
-        `self` is this unit sink.
+        `self` is this instruction sink.
         `util_info` is the unit utilization information.
         `mem_busy` is the memory busy flag.
-        The method returns the destination unit filling status.
+        The method returns the sink filling status.
+
+        """
+        return self._fill(self._get_candidates(util_info), util_info, mem_busy)
+
+    def _get_candidates(self, util_info: BagValDict[
+            ICaseString, InstrState]) -> Collection[HostedInstr]:
+        """Find candidate instructions in the donors of this sink.
+
+        `self` is this instruction sink.
+        `util_info` is the unit utilization information.
+
+        """
+        candidates = map(
+            lambda pred: _get_new_guests(pred, more_itertools.locate(
+                util_info[pred], self._valid_candid)), self._donors)
+        return self._pick_guests(
+            itertools.chain.from_iterable(candidates), util_info)
+
+    @abstractmethod
+    def _accepts_cap(self, instr: int) -> bool:
+        """Check if the given instruction capability may be accepted.
+
+        `self` is this instruction sink.
+        `instr` is the instruction to evaluate the acceptance of whose
+                capability.
+
+        """
+
+    @abstractmethod
+    def _fill(self, candidates: Collection[HostedInstr], util_info: BagValDict[
+            ICaseString, InstrState], mem_busy: bool) -> UnitFillStatus:
+        """Fill this sink.
+
+        `self` is this instruction sink.
+        `candidates` are a list of candidate instructions.
+        `util_info` is the unit utilization information.
+        `mem_busy` is the memory busy flag.
+
+        """
+
+    @abstractmethod
+    def _pick_guests(
+            self, candidates: Iterable[HostedInstr], util_info:
+            BagValDict[ICaseString, InstrState]) -> Collection[HostedInstr]:
+        """Pick the instructions to be accepted.
+
+        `self` is this instruction sink.
+        `candidates` are a list of candidate instructions.
+        `util_info` is the unit utilization information.
+
+        """
+
+    def _valid_candid(self, instr: InstrState) -> bool:
+        """Check if the given instruction is a good candidate.
+
+        `self` is this instruction sink.
+        `instr` is the instruction to evaluate whose acceptance chance.
+
+        """
+        return instr.stalled != StallState.DATA and self._accepts_cap(
+            instr.instr)
+
+    @abc.abstractproperty
+    def _donors(self) -> Iterator[ICaseString]:
+        """Retrieve the names of the units ready to supply instructions.
+
+        `self` is this instruction sink.
 
         """
 
@@ -109,36 +175,12 @@ class OutSink(InstrSink):
 
     """Dummy sink for flushing output ports"""
 
-    def fill_unit(self, util_info: BagValDict[ICaseString, InstrState],
-                  mem_busy: bool) -> UnitFillStatus:
-        """Fill an output with instructions from its predecessors.
-
-        `self` is this unit sink.
-        `util_info` is the unit utilization information.
-        `mem_busy` is the memory busy flag.
-        The method returns the destination unit filling status.
-
-        """
-        return self._fill(self._get_candidates(util_info), util_info, mem_busy)
-
-    def _get_candidates(self, util_info: BagValDict[
-            ICaseString, InstrState]) -> Collection[HostedInstr]:
-        """Find candidate instructions in the predecessors of a unit.
-
-        `self` is this unit sink.
-        `util_info` is the unit utilization information.
-
-        """
-        candidates = map(lambda pred: _get_new_guests(
-            pred, locate(util_info[pred], self._valid_candid)), self._donors)
-        return self._pick_guests(chain.from_iterable(candidates), util_info)
-
     # pylint: disable=unused-argument
     # pylint: disable=no-self-use
     def _accepts_cap(self, instr: int) -> bool:
         """Always accept all instructions.
 
-        `self` is this unit sink.
+        `self` is this output sink.
         `instr` is unused.
 
         """
@@ -146,12 +188,12 @@ class OutSink(InstrSink):
 
     def _fill(self, candidates: Collection[HostedInstr], util_info: BagValDict[
             ICaseString, InstrState], mem_busy: bool) -> UnitFillStatus:
-        """Fill the underlying unit.
+        """Commit all candidate instructions to the output.
 
-        `self` is this unit sink.
+        `self` is this output sink.
         `candidates` are a list of candidate instructions.
-        `util_info` is the unit utilization information.
-        `mem_busy` is the memory busy flag.
+        `util_info` is unused.
+        `mem_busy` is unused.
 
         """
         return UnitFillStatus(candidates, False)
@@ -159,32 +201,22 @@ class OutSink(InstrSink):
     def _pick_guests(
             self, candidates: Iterable[HostedInstr], util_info:
             BagValDict[ICaseString, InstrState]) -> Collection[HostedInstr]:
-        """Pick the instructions to be accepted.
+        """Pick all prospective instructions unconditionally.
 
-        `self` is this unit sink.
+        `self` is this output sink.
         `candidates` are a list of candidate instructions.
-        `util_info` is the unit utilization information.
+        `util_info` is unused.
 
         """
         return tuple(candidates)
     # pylint: enable=no-self-use
     # pylint: enable=unused-argument
 
-    def _valid_candid(self, instr: InstrState) -> bool:
-        """Check if the given instruction is a good candidate.
-
-        `self` is this unit sink.
-        `instr` is the instruction to evaluate whose acceptance chance.
-
-        """
-        return instr.stalled != StallState.DATA and self._accepts_cap(
-            instr.instr)
-
     @property
     def _donors(self) -> Iterator[ICaseString]:
-        """Retrieve the names of the units ready to supply instructions.
+        """Retrieve the names of the output units.
 
-        `self` is this unit sink.
+        `self` is this output sink.
 
         """
         return self._out_ports
@@ -206,30 +238,6 @@ class _InstrMovStatus:
 class UnitSink(InstrSink):
 
     """Instruction sink wrapper for functional units"""
-
-    def fill_unit(self, util_info: BagValDict[ICaseString, InstrState],
-                  mem_busy: bool) -> UnitFillStatus:
-        """Fill an output with instructions from its predecessors.
-
-        `self` is this unit sink.
-        `util_info` is the unit utilization information.
-        `mem_busy` is the memory busy flag.
-        The method returns the destination unit filling status.
-
-        """
-        return self._fill(self._get_candidates(util_info), util_info, mem_busy)
-
-    def _get_candidates(self, util_info: BagValDict[
-            ICaseString, InstrState]) -> Collection[HostedInstr]:
-        """Find candidate instructions in the predecessors of a unit.
-
-        `self` is this unit sink.
-        `util_info` is the unit utilization information.
-
-        """
-        candidates = map(lambda pred: _get_new_guests(
-            pred, locate(util_info[pred], self._valid_candid)), self._donors)
-        return self._pick_guests(chain.from_iterable(candidates), util_info)
 
     def _accepts_cap(self, instr: int) -> bool:
         """Check if the given instruction capability may be accepted.
@@ -296,19 +304,9 @@ class UnitSink(InstrSink):
             key=lambda instr_info:
             util_info[instr_info.host][instr_info.index_in_host].instr)
 
-    def _valid_candid(self, instr: InstrState) -> bool:
-        """Check if the given instruction is a good candidate.
-
-        `self` is this unit sink.
-        `instr` is the instruction to evaluate whose acceptance chance.
-
-        """
-        return instr.stalled != StallState.DATA and self._accepts_cap(
-            instr.instr)
-
     @property
     def _donors(self) -> Iterator[ICaseString]:
-        """Retrieve the names of the units ready to supply instructions.
+        """Retrieve the predecessor names.
 
         `self` is this unit sink.
 
