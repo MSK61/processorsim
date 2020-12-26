c95e002aa1ff4a192611c0f712ef2029827b168b
Mohammed El-Afifi <Mohammed_ElAfifi@yahoo.com>
2020-12-26 23:53:58 +0200

    Add an example execution test
    Because:
    The example has been outdated for some time without being noticed.
    
    This test should help highlight the problem of the example getting
    outdated at any time later.

---
+++ tests/test_examples.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""examples execution tests"""
+
+############################################################
+#
+# Copyright 2017, 2019, 2020 Mohammed El-Afifi
+# This file is part of processorSim.
+#
+# processorSim is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# processorSim is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with processorSim.  If not, see
+# <http://www.gnu.org/licenses/>.
+#
+# program:      processor simulator
+#
+# file:         test_examples.py
+#
+# function:     examples execution tests
+#
+# description:  makes sure examples don't get outdated
+#
+# author:       Mohammed El-Afifi (ME)
+#
+# environment:  Visual Studdio Code 1.52.1, python 3.8.6, Fedora release
+#               33 (Thirty Three)
+#
+# notes:        This is a private program.
+#
+############################################################
+
+from os.path import join
+import unittest
+
+import nbconvert.preprocessors
+import nbformat
+
+import test_env
+
+
+class ExampleTest(unittest.TestCase):
+
+    """Test case for fulfilling complete code coverage"""
+
+    def test_example(self):
+        """Test example execution.
+
+        `self` is this test case.
+
+        """
+        examples_dir = join(test_env.TEST_DIR, "../examples")
+        with open(join(examples_dir, "unified memory.ipynb")) as nb_file:
+            nbconvert.preprocessors.ExecutePreprocessor().preprocess(
+                nbformat.read(nb_file, nbformat.NO_CONVERT),
+                {"metadata": {"path": examples_dir}})
+
+
+def main():
+    """entry point for running test in this module"""
+    unittest.main()
+
+
+if __name__ == '__main__':
+    main()
